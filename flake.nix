fa71e36f1267ef0c9ec0975a3ae6e075b1dd3d18
Florian Unterweger <f.unterweger@student.tugraz.at>
2025-08-03 16:31:00 +0200

    home manager module

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
   };
 
   outputs = { self, nixpkgs, ... }:
@@ -8,21 +9,52 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      package = self.packages.${system}.default;
       packages.${system}.default = pkgs.stdenv.mkDerivation {
         pname = "audio-systray";
         version = "1.0";
         src = ./.;
         buildInputs = with pkgs; [
-          cmake pkg-config gtk3 xapp.dev pulseaudio libnotify xorg.libX11
+          cmake pkg-config gtk3 xapp.dev pulseaudio libnotify xorg.libX11 makeWrapper
         ];
-        installPhase = ''
-          make
-          mkdir -p $out/bin
-          cp audio_systray $out/bin/audio-systray
+
+        postInstall = ''
+          wrapProgram $out/bin/audio-systray \
+            --prefix PATH : ${pkgs.pavucontrol}/bin \
+            --prefix PATH : ${pkgs.playerctl}/bin \
         '';
       };
+
+      homeManagerModules = {
+        default = { config, lib, ... }: {
+          options = {
+            services.audio-systray.enable = lib.mkEnableOption "Enable audio-systray user service";
+          };
+
+          config = lib.mkIf config.services.audio-systray.enable {
+            systemd.user.services.audio-systray = {
+              Unit = {
+                Description = "audio systray";
+                Requires = [ "tray.target" ];
+                After = [
+                  "graphical-session.target"
+                    "tray.target"
+                ];
+                PartOf = [ "graphical-session.target" ];
+              };
+
+              Install = {
+                WantedBy = [ "graphical-session.target" ];
+              };
+
+              Service = {
+                ExecStart = "${self.packages.${system}.default}/bin/audio-systray"; 
+                Restart = "on-failure";
+              };
+            };
+          };
+        };
+      };
+
     };
 }
-
 
